400ce5491501703a2d7b3c3dccbf648f329838eb
Fame Data <patcharapol.yasamut@gmail.com>
2024-06-09 07:32:49 +0700

    add scr file

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        require = file_obj.readlines()
+        require = [req.replace('\n', '') for req in require]
+        
+        if HYPEN_E_DOT in require:
+            require.remove(HYPEN_E_DOT)
+
+setup(
+    name='mlproject1',
+    version='0.0.1',
+    author='Fame_Patcharapol',
+    author_email='patcharapol.yasamut@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas']
+    
+)
